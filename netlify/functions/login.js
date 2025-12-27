1cbb1d3c581d781835be3a3519ec1fde5d876687
akash__dp <117759810+DP7772@users.noreply.github.com>
2025-12-27 20:32:14 +0530

    login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,12 +1,14 @@
-// netlify/functions/login.js
-import { neon } from '@netlify/neon';
-const sql = neon();
+import { neon } from '@neondatabase/serverless';
+
+// Explicitly pass YOUR variable name
+const sql = neon(process.env.NETLIFY_DATABASE_URL);
 
 export async function handler(event) {
   try {
     const { email, password } = JSON.parse(event.body);
 
     const user = await sql`SELECT * FROM users WHERE email=${email} AND password=${password}`;
+    
     if(user.length > 0) {
       return { statusCode: 200, body: JSON.stringify({status:'success'}) };
     } else {
