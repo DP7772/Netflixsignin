d2cf58f98b52e7ecd89583ca271d5165c2fb84b7
akash__dp <117759810+DP7772@users.noreply.github.com>
2025-12-27 19:12:04 +0530

    login.js

---
+++ netlify/functions/login.js
@@ -0,0 +1,19 @@
+// netlify/functions/login.js
+import { neon } from '@netlify/neon';
+const sql = neon();
+
+export async function handler(event) {
+  try {
+    const { email, password } = JSON.parse(event.body);
+
+    const user = await sql`SELECT * FROM users WHERE email=${email} AND password=${password}`;
+    if(user.length > 0) {
+      return { statusCode: 200, body: JSON.stringify({status:'success'}) };
+    } else {
+      return { statusCode: 200, body: JSON.stringify({status:'fail'}) };
+    }
+
+  } catch(err) {
+    return { statusCode: 500, body: JSON.stringify({status:'error', message: err.message}) };
+  }
+}
